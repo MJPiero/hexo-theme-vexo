d911d1b35ee22fe3f2c69fb7eb7963a779123886
MaJing <merlu.majng@gmail.com>
2018-08-24 12:03:05 +0800

    针对MOB主题调整

--- source/js/script.js
+++ source/js/script.js
@@ -11,7 +11,7 @@
   $(document).ready(function () {
     NProgress.start()
     $('#nprogress .bar').css({
-      'background': '#42b983'
+      'background': '#ff6600'
     })
     $('#nprogress .spinner').hide()
 
